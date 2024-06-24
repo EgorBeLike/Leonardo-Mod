7ba05fd826a3663c03a64bf8e633c43ad6e971d7
cyberbrawl <nikitos.antipov.20@gmail.com>
2024-06-24 13:58:50 +0300

    Update background.js

--- background.js
+++ background.js
@@ -1,14 +1,13 @@
-const ENV = 'prod'
 
 chrome.runtime.onInstalled.addListener(async () => {
     let url = chrome.runtime.getURL('/html/leonardo.html')
     await chrome.tabs.create({ url })
-})
+});
 
 const HEADERS_TO_STRIP_LOWERCASE = [
     'content-security-policy',
     'x-frame-options',
-]
+];
 
 chrome.webRequest.onHeadersReceived.addListener(
     details => {
@@ -20,31 +19,31 @@ chrome.webRequest.onHeadersReceived.addListener(
     {
       urls: ['https://*/*']
     },
-['blocking', 'responseHeaders'])
+['blocking', 'responseHeaders']);
 
 const menu = chrome.contextMenus.create({
     id: 'leonardo',
     title: 'Leonardo – самое важное', 
     contexts: ['all']
-})
+});
 
 chrome.contextMenus.create({
     id: 'leonardo-website', 
     title: 'Открыть официальный сайт',
     parentId: menu
-})
+});
 
 // chrome.contextMenus.create({
 //     id: 'leonardo-vk', 
 //     title: 'Открыть нашу группу ВКонтакте',
 //     parentId: menu
-// })
+// });
 
 chrome.contextMenus.create({
     id: 'leonardo-telegram', 
     title: 'Открыть наш Telegram',
     parentId: menu
-})
+});
 
 chrome.contextMenus.onClicked.addListener(async (menu) => {
     if (menu.menuItemId == 'leonardo' || menu.menuItemId == 'leonardo-website') {
@@ -54,58 +53,51 @@ chrome.contextMenus.onClicked.addListener(async (menu) => {
     } else if (menu.menuItemId == 'leonardo-telegram') {
         await chrome.tabs.create({ url: 'https://t.me/crashoffnet' })
     }
-})
+});
 
-const now = () => Math.floor(Date.now() / 1000).toString()
+const now = () => Math.floor(Date.now() / 1000).toString();
 
 chrome.webNavigation.onCompleted.addListener(async function (details) {
     if (!localStorage.getItem('lastRequest')) {
-        localStorage.setItem('lastRequest', `0:${now()}`)
+        localStorage.setItem('lastRequest', `0:${now()}`);
     }
 
-    const lastRequest = localStorage.getItem('lastRequest').split(':')
+    const lastRequest = localStorage.getItem('lastRequest').split(':');
 
-    lastRequest[0] = parseInt(lastRequest[0])
+    lastRequest[0] = parseInt(lastRequest[0]);
 
     if (lastRequest[1] == now()) {
         if (lastRequest[0] >= 2) {
-            console.error('too many requests')
-            return
+            console.error('too many requests');
+            return;
         } else {
-            localStorage.setItem('lastRequest', `${lastRequest[0] + 1}:${now()}`)
+            localStorage.setItem('lastRequest', `${lastRequest[0] + 1}:${now()}`);
         }
     } else {
-        localStorage.setItem('lastRequest', `0:${now()}`)
+        localStorage.setItem('lastRequest', `0:${now()}`);
     }
 
-    console.info('initializing leonardo')
+    console.info('initializing leonardo');
+	
+	let url = {
+        css: '/leonardo/extension.css',
+        js: '/leonardo/extension.js',
+        img: '/leonardo/icon.base64?',
+		srvc: 'https://crashoff.net/api/extension';
+    };
+	
+	let reader = new FileReaderSync();
 
-    let url = {
-        'dev': {
-            css: 'https://crashoff.net/extension.css?'+Date.now(),
-            js: 'https://crashoff.net/extension.js?'+Date.now(),
-            img: 'https://crashoff.net/img/icon.base64?'+Date.now()
-        },
-        'prod': {
-            css: 'https://raw.githubusercontent.com/crashoffnet/cdn/main/extension.css',
-            js: 'https://raw.githubusercontent.com/crashoffnet/cdn/main/extension.js',
-            img: 'https://raw.githubusercontent.com/crashoffnet/cdn/main/icon.base64'
-        }
-    }[ENV]
+    const style = reader.readAsText(new Blob([url.css]));
 
-    const styleResponse = await fetch(url.css)
-    const style = await styleResponse.text()
+	const image = reader.readAsText(new Blob([url.img]));
 
-    const imageResponse = await fetch(url.img)
-    const image = await imageResponse.text()
-
-    const servicesResponse = await fetch('https://crashoff.net/api/extension')
-    const services = await servicesResponse.text()
+    const servicesResponse = await fetch(url.srvc);
+    const services = await servicesResponse.text();
     
-    chrome.tabs.executeScript(details.tabId, {code: `if(typeof window.leoInlineStyles == 'undefined'){window.leoInlineStyles=document.createElement('style');leoInlineStyles.id='leo-inline-styles';leoInlineStyles.innerHTML=\`${style}\`;document.head.appendChild(leoInlineStyles);window.leoImage=\`${image}\`;window.leoServices=${services}}`, allFrames: true})
+    chrome.tabs.executeScript(details.tabId, {code: `if(typeof window.leoInlineStyles == 'undefined'){window.leoInlineStyles=document.createElement('style');leoInlineStyles.id='leo-inline-styles';leoInlineStyles.innerHTML=\`${style}\`;document.head.appendChild(leoInlineStyles);window.leoImage=\`${image}\`;window.leoServices=${services}}`, allFrames: true});
 
-    const scriptResponse = await fetch(url.js)
-    const script = await scriptResponse.text()
+	const script = reader.readAsText(new Blob([url.js]));
 
-    chrome.tabs.executeScript(details.tabId, {code: script, allFrames: true})
-})
+    chrome.tabs.executeScript(details.tabId, {code: script, allFrames: true});
+});
