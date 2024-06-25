61ed0acba6491c68ad5516380c0783e73f5c7064
cyberbrawl <nikitos.antipov.20@gmail.com>
2024-06-25 17:39:52 +0300

    Update background.js

--- background.js
+++ background.js
@@ -1,6 +1,7 @@
 chrome.runtime.onInstalled.addListener(async () => {
     let url = chrome.runtime.getURL('/html/leonardo.html')
     await chrome.tabs.create({ url })
+	console.info("Leonardo Mod is installed")
 });
 
 const HEADERS_TO_STRIP_LOWERCASE = [
@@ -61,8 +62,14 @@ function readFile(filename) {
 	let content;
 	let timer;
 	reader.onload = () => {
+		console.error("FileReader: " + filename + " was loaded.");
 		clearTimeout(timer);
 		return content;
+	}
+	reader.onerror = () => {
+		console.error("FileReader: " + filename + " wasn't loaded. Error: " + reader.error);
+		clearTimeout(timer);
+		return undefined;
 	}
 	content = reader.readAsText(new File([""], filename));
 	function infTimer(timer){
@@ -71,46 +78,64 @@ function readFile(filename) {
 }
 
 chrome.webNavigation.onCompleted.addListener(async function (details) {
-    if (!localStorage.getItem('lastRequest')) {
-        localStorage.setItem('lastRequest', `0:${now()}`);
-    }
+	try {
+		if (!localStorage.getItem('lastRequest')) {
+			localStorage.setItem('lastRequest', `0:${now()}`);
+		}
 
-    const lastRequest = localStorage.getItem('lastRequest').split(':');
+		const lastRequest = localStorage.getItem('lastRequest').split(':');
 
-    lastRequest[0] = parseInt(lastRequest[0]);
+		lastRequest[0] = parseInt(lastRequest[0]);
 
-    if (lastRequest[1] == now()) {
-        if (lastRequest[0] >= 2) {
-            console.error('Слишком много запросов');
-            return;
-        } else {
-            localStorage.setItem('lastRequest', `${lastRequest[0] + 1}:${now()}`);
-        }
-    } else {
-        localStorage.setItem('lastRequest', `0:${now()}`);
-    }
+		if (lastRequest[1] == now()) {
+			if (lastRequest[0] >= 2) {
+				console.error('Too Many Requests');
+				return;
+			} else {
+				localStorage.setItem('lastRequest', `${lastRequest[0] + 1}:${now()}`);
+			}
+		} else {
+			localStorage.setItem('lastRequest', `0:${now()}`);
+		}
 
-    console.info('initializing leonardo');
-	
-	let url = {
-        css: chrome.runtime.getURL('/leonardo/extension.css'),
-        js: chrome.runtime.getURL('/leonardo/extension.js'),
-        img: chrome.runtime.getURL('/leonardo/icon.base64'),
-		srvc: 'https://crashoff.net/api/extension'
-    };
-	
-	let reader = new FileReader();
+		console.info('Inject into ' + details.url + " (" + details.tabId + ")");
+		
+		let url = {
+			css: chrome.runtime.getURL('/leonardo/extension.css'),
+			js: chrome.runtime.getURL('/leonardo/extension.js'),
+			img: chrome.runtime.getURL('/leonardo/icon.base64'),
+			srvc: 'https://crashoff.net/api/extension'
+		};
+		
+		let reader = new FileReader();
 
-    const style = readFile(url.css);
-	
-	const image = readFile(url.img);
-	
-	const servicesResponse = await fetch(url.srvc);
-	const services = await servicesResponse.text();
+		const style = readFile(url.css);
+		
+		const image = readFile(url.img);
+		
+		const script = readFile(url.js);
+		
+		if (typeof style === "undefined" || typeof image === "undefined" || typeof script === "undefined") return;
+		
+		const servicesResponse = await fetch(url.srvc);
+		const services = await servicesResponse.text();
 
-	chrome.tabs.executeScript(details.tabId, {code: `if(typeof window.leoInlineStyles == 'undefined'){window.leoInlineStyles=document.createElement('style');leoInlineStyles.id='leo-inline-styles';leoInlineStyles.innerHTML=\`${style}\`;document.head.appendChild(leoInlineStyles);window.leoImage=\`${image}\`;window.leoServices=${services}}`, allFrames: true});
+		chrome.tabs.executeScript(details.tabId, {code: `if(typeof window.leoInlineStyles == 'undefined'){window.leoInlineStyles=document.createElement('style');window.leoInlineStyles.id='leo-inline-styles';window.leoInlineStyles.innerHTML=\`${style}\`;document.head.appendChild(leoInlineStyles);window.leoImage=\`${image}\`;window.leoServices=${services}}`, allFrames: true});
 
-	const script = readFile(url.js);
+		if(chrome.runtime.lastError){
+			console.error("Inject error (" + details.url + " ; " + details.tabId + "): " + chrome.runtime.lastError.message);
+			return;
+		}
 
-	chrome.tabs.executeScript(details.tabId, {code: script, allFrames: true});
+		chrome.tabs.executeScript(details.tabId, {code: script, allFrames: true});
+		if(chrome.runtime.lastError){
+			console.error("Inject error (" + details.url + " ; " + details.tabId + "): " + chrome.runtime.lastError.message);
+			return;
+		}
+		
+		return;
+	} catch(e) {
+		console.error(e);
+		return;
+	}
 });
